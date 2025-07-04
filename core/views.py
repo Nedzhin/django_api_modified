afd28627297b52a0018ee97e516f494b54dd4ecc
Nurseiit <nurseiit@Nurseiits-MacBook-Air.local>
2025-07-04 04:46:46 +0100

    added last code changes

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework as filters
-#from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 from .models import Instruction, GPSurgery
 from .serializers import InstructionSerializer
 
--- gpinstructions/settings.py
+++ gpinstructions/settings.py
@@ -1,5 +1,7 @@
+from pathlib import Path
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
+    'django.contrib.staticfiles', # requires static file handling
     'django.contrib.auth',
     'rest_framework',
     'django_filters',
@@ -13,8 +15,54 @@ DATABASES = {
         'NAME': 'db.sqlite3',
     }
 }
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+WSGI_APPLICATION = 'gpinstructions.wsgi.application'
 
 SECRET_KEY = 'dummy'
 ROOT_URLCONF = 'gpinstructions.urls'
 DEBUG = True
 ALLOWED_HOSTS = []
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,  # this must be True to find DRF's internal templates
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SearchFilter',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+}
